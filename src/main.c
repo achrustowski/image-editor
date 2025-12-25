a5386c22e4aab5fcf3d1f3cb5c73ca4aef6eb4eb
achrustowski <99372096+achrustowski@users.noreply.github.com>
2025-12-25 13:23:38 +0100

    implement real time crop area

--- src/main.c
+++ src/main.c
@@ -1,16 +1,67 @@
 #include <stdio.h>
 #include <raylib.h>
+#include <stdlib.h>
+#include <raymath.h>
 
 #define S_W     1024
 #define S_H     768
 
 Texture2D texture = {0};
+bool is_mouse_pressed = false;
+
+bool is_vector_zero(Vector2 v)
+{
+    if (v.x == 0 && v.y == 0)
+    {
+        return true;
+    } else
+    {
+        return false;
+    }
+}
 
 void load_image(const char* filename)
 {
     Image image = LoadImage(filename);
     texture = LoadTextureFromImage(image);
     UnloadImage(image);
+}
+
+Rectangle generate_crop_area()
+{
+    static Vector2 start = {0};
+    static Vector2 end = {0};
+    static bool dragging = false;
+
+    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+    {
+        start = GetMousePosition();
+        end = start;
+        dragging = true;
+    }
+
+    if (dragging)
+    {
+        end = GetMousePosition();
+    }
+
+    if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON))
+    {
+        dragging = false;
+    }
+
+    float x = fminf(start.x, end.x);
+    float y = fminf(start.y, end.y);
+    float w = fabsf(start.x - end.x);
+    float h = fabsf(start.y - end.y);
+
+    if (w == 0 || h == 0)
+    {
+        return (Rectangle) {0};
+    } else
+    {
+        return (Rectangle) {x, y, w, h};
+    }
 }
 
 int main()
@@ -53,9 +104,11 @@ int main()
         {
             SetWindowSize(texture.width, texture.height);
         }
+        
         BeginDrawing();
         ClearBackground(BLACK);
         DrawTexture(texture, 0, 0, WHITE);
+        DrawRectangleLinesEx(generate_crop_area(), 1.0f, RED);
         EndDrawing();
     }
 
